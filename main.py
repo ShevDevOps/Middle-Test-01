d934ba58c74b40e564cdb2010b192b0c4115914f
shed-crypto <alexey.rachenko@gmail.com>
2025-03-28 18:34:34 +0200

    Conforming to PEP8 and quick fix

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 import os
 from typing import Iterable, Set
 
+
 def read_file(file_path: str) -> Set[str]:
     """
     Reads a file and returns a set lines.
+
     Parameters:
         file_path (str): Path to the file.
     Returns:
@@ -13,10 +15,11 @@ def read_file(file_path: str) -> Set[str]:
     """
     if not os.path.exists(file_path):
         raise FileNotFoundError(f"Файл {file_path} не знайдено.")
-    
-    with open(file_path, 'r', encoding='utf-8') as file:
+
+    with open(file_path, "r", encoding="utf-8") as file:
         return set(file.read().splitlines())
-    
+
+
 def same(lines1, lines2):
     result = []
     for line1 in lines1:
@@ -25,6 +28,7 @@ def same(lines1, lines2):
                 result.append(line2)
                 break
     return result
+
 
 def diff(first_file: list[str], second_file: list[str]) -> list[str]:
     unique_lines = []
@@ -35,27 +39,29 @@ def diff(first_file: list[str], second_file: list[str]) -> list[str]:
     for line in second_file:
         if line not in first_file:
             unique_lines.append(line)
-    
+
     return unique_lines
 
 
 def write_to_file(file_path: str, lines: Iterable[str]) -> None:
     """
     Writes a list of lines to a file, separating them with newline characters.
+
     Parameters:
         file_path (str): Path to the file where data will be written.
         lines (Iterable[str]): A collection of strings to write to the file.
     Returns:
         None
-    """ 
-    with open(file_path, 'w', encoding='utf-8') as file:
-        file.write('\n'.join(lines))
+    """
+    with open(file_path, "w", encoding="utf-8") as file:
+        file.write("\n".join(lines))
 
-def main(): 
-    file1 = 'file1.txt'
-    file2 = 'file2.txt'
-    same_output = 'same.txt'
-    diff_output = 'diff.txt'
+
+def main():
+    file1 = "file1.txt"
+    file2 = "file2.txt"
+    same_output = "same.txt"
+    diff_output = "diff.txt"
     try:
         lines1 = read_file(file1)
         lines2 = read_file(file2)
@@ -68,7 +74,6 @@ def main():
     except FileNotFoundError as e:
         print(e)
 
+
 if __name__ == "__main__":
     main()
-
-
--- tests/test_rw_files.py
+++ tests/test_rw_files.py
@@ -2,28 +2,32 @@ import pytest
 from typing import List
 
 from main import read_file, write_to_file
- 
+
+
 @pytest.fixture
 def setup_files(tmpdir) -> tuple[str, str]:
     """Creates test files for verification."""
     testfile1 = tmpdir.join("test1.txt")
     testfile2 = tmpdir.join("test2.txt")
-    
+
     testfile1.write("\n".join(["line1", "line2", "line3"]))
     testfile2.write("\n".join(["line2", "line3", "line4"]))
-    
+
     return str(testfile1), str(testfile2)
 
-def test_read_file_existing(setup_files): 
+
+def test_read_file_existing(setup_files):
     """Test the initial setup files."""
     testfile1, _ = setup_files
     lines = read_file(testfile1)
     assert lines == {"line1", "line2", "line3"}
-  
+
+
 def test_read_file_not_found() -> None:
     """Checks that if the file does not exist, a FileNotFoundError is raised."""
     with pytest.raises(FileNotFoundError):
         read_file("non_existent_file.txt")
+
 
 def test_read_file_empty(tmpdir) -> None:
     """Test that the function `read_file` returns an empty set for an empty file."""
@@ -32,13 +36,16 @@ def test_read_file_empty(tmpdir) -> None:
     lines = read_file(str(empty_file))
     assert lines == set()
 
-@pytest.mark.parametrize("lines_to_write", [
-    (["line1", "line2", "line3"]),
-    (["apple", "banana", "cherry"]),
-    ([]),
-    (["line1", "line2", "line3", "line1"])
-])
- 
+
+@pytest.mark.parametrize(
+    "lines_to_write",
+    [
+        (["line1", "line2", "line3"]),
+        (["apple", "banana", "cherry"]),
+        ([]),
+        (["line1", "line2", "line3", "line1"]),
+    ],
+)
 def test_write_to_file_param(tmpdir, lines_to_write: List[str]) -> None:
     """Verifies writing to a file for various inputs."""
     output_file = tmpdir.join("output_param.txt")
