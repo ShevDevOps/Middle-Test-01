7f01f602c0f3734e2d0e56ec46ba1287a0b75e27
shed-crypto <alexey.rachenko@gmail.com>
2025-03-28 17:42:57 +0200

    Add test_read_file_existing with pytest.fixture.

--- tests/test_rw_files.py
+++ tests/test_rw_files.py
@@ -1,7 +1,24 @@
 import pytest 
 
-from main import read_file
+from main import read_file 
+ 
+@pytest.fixture
+def setup_files(tmpdir) -> tuple[str, str]:
+    """Creates test files for verification."""
+    testfile1 = tmpdir.join("test1.txt")
+    testfile2 = tmpdir.join("test2.txt")
+    
+    testfile1.write("\n".join(["line1", "line2", "line3"]))
+    testfile2.write("\n".join(["line2", "line3", "line4"]))
+    
+    return str(testfile1), str(testfile2)
 
+def test_read_file_existing(setup_files): 
+    """Test the initial setup files."""
+    testfile1, _ = setup_files
+    lines = read_file(testfile1)
+    assert lines == {"line1", "line2", "line3"}
+  
 def test_read_file_not_found() -> None:
     """Checks that if the file does not exist, a FileNotFoundError is raised."""
     with pytest.raises(FileNotFoundError):
